52f1d091517b715130ed41c2d7944a11b8cd829b
bwburnsides <19192452+bwburnsides@users.noreply.github.com>
2024-02-18 22:08:01 -0600

    outstanding work

---
+++ bw8.asm
@@ -0,0 +1,195 @@
+#bankdef isa { #bits 8 }
+
+#subruledef gpr {
+    r0  => 0b0000
+    r1  => 0b0001
+    r2  => 0b0010
+    r3  => 0b0011
+    r4  => 0b0100
+    r5  => 0b0101
+    r6  => 0b0110
+    r7  => 0b0111
+    r8  => 0b1000
+    r9  => 0b1001
+    r10 => 0b1010
+    r11 => 0b1011
+    r12 => 0b1100
+    r13 => 0b1101
+    r14 => 0b1110
+    r15 => 0b1111
+
+    lr => r15
+}
+
+#subruledef vec {
+    v0 => 0b000
+    v1 => 0b001
+    v2 => 0b010
+    v3 => 0b011
+    v4 => 0b100
+    v5 => 0b101
+    v6 => 0b110
+    v7 => 0b111
+}
+
+#subruledef mmu {
+    mmu0  => 0b0000
+    mmu1  => 0b0001
+    mmu2  => 0b0010
+    mmu3  => 0b0011
+    mmu4  => 0b0100
+    mmu5  => 0b0101
+    mmu6  => 0b0110
+    mmu7  => 0b0111
+    mmu8  => 0b1000
+    mmu9  => 0b1001
+    mmu10 => 0b1010
+    mmu11 => 0b1011
+    mmu12 => 0b1100
+    mmu13 => 0b1101
+    mmu14 => 0b1110
+    mmu15 => 0b1111
+}
+
+#subruledef spr {
+    ilr => 0b0
+    psr => 0b1
+}
+
+#subruledef lui_imm {
+    {imm: i16} => {
+        assert(imm & 0b11111 == 0x00)
+        imm
+    }
+}
+
+; TODO: Rename
+#subruledef mem16_imm {
+    {imm: s5} => {
+        assert(imm & 0b1 == 0x00)
+        imm
+    }
+}
+
+#subruledef stz_imm {
+    {imm: u5} => {
+        assert(imm & 0b1 == 0x00)
+        imm
+    }
+}
+
+#subruledef jr_imm {
+    {imm: s7} => {
+        assert(imm & 0b1 == 0x00)
+        imm
+    }
+}
+
+#subruledef func {
+    mv   => 0b0000
+    add  => 0b0001
+    addc => 0b0010
+    sub  => 0b0011
+    subb => 0b0100
+    and  => 0b0101
+    or   => 0b0110
+    xor  => 0b0111
+    shl  => 0b1000
+    lsr  => 0b1001
+    asr  => 0b1010
+    cmp  => 0b1011
+}
+
+#subruledef op11 {
+    lli  => 0b00
+    addi => 0b01
+    cmpi => 0b10
+}
+
+#subruledef jump {
+    j  => 0b0
+    jal => 0b1
+}
+
+#subruledef jump_rel {
+    jr     => 0b000
+    jral   => 0b001
+    br.eq  => 0b010
+    br.ne  => 0b011
+    br.lt  => 0b100
+    br.ge  => 0b101
+    br.lts => 0b110
+    br.ges => 0b111
+}
+
+#subruledef bitwise_imm {
+    andi => 0b0
+    ori => 0b1
+}
+
+#subruledef unary {
+    not => 0b000
+    apc => 0b001
+    neg => 0b010
+    negb => 0b011
+    swap => 0b100
+    sxt => 0b101
+}
+
+#subruledef int {
+    sys => 0b0
+    irq => 0b1
+}
+
+#subruledef nullary {
+    reti => 0b000
+}
+
+#ruledef bw8 {
+    lui {rd: gpr}, {imm: lui_imm} => 0b0 @ imm[14:9] @ imm[15:15] @ imm[8:5] @ rd
+
+    ld8 {rd: gpr}, {rs: gpr}, {imm: s5} => 0b10 @ 0b0 @ imm[0:0] @ imm[3:1] @ imm[4:4] @ rs @ rd
+    st8 {rs: gpr}, {rd: gpr}, {imm: s5} => 0b10 @ 0b1 @ imm[0:0] @ imm[3:1] @ imm[4:4] @ rs @ rd
+
+    ld {rd: gpr}, {rs: gpr}, {imm: mem16_imm} => 0b110 @ 0b0 @ imm[3:1] @ imm[4:4] @ rs @ rd
+    st {rs: gpr}, {rd: gpr}, {imm: mem16_imm} => 0b110 @ 0b1 @ imm[3:1] @ imm[4:4] @ rs @ rd
+
+    {func: func} {rd: gpr}, {rs: gpr} => 0b1110 @ func @ rs @ rd
+
+    mv {vd: vec}, {rs: gpr} => 0b1110 @ 0b1100 @ 0b0 @ vd @ rs
+    mv {rd: gpr}, {vs: vec} => 0b1110 @ 0b1100 @ 0b1 @ vs @ rd
+
+    shli {rd: gpr}, {imm: u4} => 0b1110 @ 0b1101 @ imm[3:0] @ rd
+    lsri {rd: gpr}, {imm: u4} => 0b1110 @ 0b1110 @ imm[3:0] @ rd
+    asri {rd: gpr}, {imm: u4} => 0b1110 @ 0b1111 @ imm[3:0] @ rd
+
+    {func: op11} {rd: gpr}, {imm: i5} => 0b1111 @ 0b0 @ func @ imm[4:0] @ rd
+
+    {j: jump} {rs: gpr}, {imm: mem16_imm} => 0b1111 @ 0b0 @ 0b11 @ imm[4:4] @ rs @ j @ imm[3:1]
+
+    mv {md: mmu}, {rs: gpr} => 0b1111 @ 0b10 @ 0b0 @ 0b0 @ md @ rs
+    mv {rd: gpr}, {ms: mmu} => 0b1111 @ 0b10 @ 0b0 @ 0b1 @ ms @ rd
+
+    {jr: jump_rel} {imm: jr_imm} => 0b1111 @ 0b10 @ 0b1 @ imm[6:6] @ imm[3:1] @ imm[5:4] @ jr
+
+    {func: bitwise_imm} {rd: gpr}, {imm: u4} => 0b1111 @ 0b110 @ func @ imm[3:0] @ rd
+
+    stz {rs: gpr}, {imm: stz_imm} => 0b1111 @ 0b1110 @ rs @ imm[4:1]
+
+    {func: unary} {rd: gpr} => 0b1111 @ 0b1111 @ 0b0 @ func @ rd
+
+    mv {sd: spr}, {rs: gpr} => 0b1111 @ 0b1111 @ 0b10 @ 0b0 @ sd @ rs
+    mv {rd: gpr}, {ss: spr} => 0b1111 @ 0b1111 @ 0b10 @ 0b1 @ ss @ rd
+
+    env {imm: u4} => 0b1111 @ 0b1111 @ 0b1110 @ imm
+
+    {int: int} {imm: u2} => 0b1111 @ 0b1111 @ 0b1111 @ 0b0 @ int @ imm
+
+    {func: nullary} => 0b1111 @ 0b1111 @ 0b1111 @ 0b1 @ func
+}
+
+#bankdef test {
+    #addr 0x0000
+    #size 0xffff
+    #outp 0
+}
--- scripts/decode.py
+++ scripts/decode.py
@@ -1,49 +1,76 @@
 import enum
 from dataclasses import dataclass
 
-# inst[8] @ inst[14:9] @ inst[7:4] @ Bit(0)*4
+
+class Register(enum.IntEnum):
+    R0 = 0
+    R1 = enum.auto()
+    R2 = enum.auto()
+    R3 = enum.auto()
+    R4 = enum.auto()
+    R5 = enum.auto()
+    R6 = enum.auto()
+    R7 = enum.auto()
+    R8 = enum.auto()
+    R9 = enum.auto()
+    R10 = enum.auto()
+    R11 = enum.auto()
+    R12 = enum.auto()
+    R13 = enum.auto()
+    R14 = enum.auto()
+    R15 = enum.auto()
+
+    LinkRegister = R15
 
 
-# class Word:
-#     def __init__(self, word: int):
-#         split = list(bin(word)[2:])
-#         while len(split) != 16:
-#             split.insert(0, "0")
-
-#         self.as_str =  "".join(split)
-
-#     def __getitem__(self, index: int) -> int
+class ImmediateFormat(enum.IntEnum):
+    Lui = 0
+    Mem = enum.auto()
+    Jmp = enum.auto()
+    Alu = enum.auto()
+    JmpR = enum.auto()
+    Stz = enum.auto()
 
 
-class MemoryWriteMode(enum.Enum):
-    Disabled = enum.auto()
+class MemoryMode(enum.IntEnum):
+    Disabled = 0
+    ReadWord = enum.auto()
+    ReadByte = enum.auto()
+    WriteWord = enum.auto()
+    WriteByte = enum.auto()
+
+
+class MemoryOperationFormat(enum.Enum):
     Word = enum.auto()
     Byte = enum.auto()
 
 
-class WritebackMode(enum.Enum):
-    Disabled = enum.auto()
+class WritebackMode(enum.IntEnum):
+    Disabled = 0
     AluResult = enum.auto()
     MemoryResult = enum.auto()
 
 
-class AluOperation(enum.Enum):
+class AluFunction(enum.IntEnum):
     Nop = 0  # TODO: Keep?
-    Lhs = 1
-    Add = 2
-    Addc = 3
-    Sub = 4
-    Subb = 5
-    And = 6
-    Or = 7
-    Xor = 8
-    Shl = 9
-    Lsr = 10
-    Asr = 11
+    Lhs = enum.auto()
+    Rhs = enum.auto()  # TODO: Both??
+    Add = enum.auto()
+    Addc = enum.auto()
+    Sub = enum.auto()
+    Subb = enum.auto()
+    And = enum.auto()
+    Or = enum.auto()
+    Xor = enum.auto()
+    Shl = enum.auto()
+    Lsr = enum.auto()
+    Asr = enum.auto()
+    Swap = enum.auto()
+    Sxt = enum.auto()
 
 
-class JumpCondition(enum.Enum):
-    Always = enum.auto()
+class JumpCondition(enum.IntEnum):
+    Always = 0
     Never = enum.auto()
     Equal = enum.auto()
     NotEqual = enum.auto()
@@ -53,101 +80,409 @@ class JumpCondition(enum.Enum):
     SignedGreaterEqual = enum.auto()
 
 
+class LeftSelect(enum.IntEnum):
+    SourceRegister = 0
+    ProgramCounter = enum.auto()
+
+
+class RightSelect(enum.IntEnum):
+    DestinationRegister = 0
+    Immediate = enum.auto()
+
+
 @dataclass
 class ControlWord:
-    MemoryReadEnable: bool  # 1
-    MemoryWriteEnable: MemoryWriteMode  # 2
-    WritebackEnable: WritebackMode  # 2
-    AluOperation: AluOperation  # 4
-    Immediate: int  # 3
-    JumpCondition: JumpCondition  # 3
+    left_select: LeftSelect = LeftSelect(0)
+    right_select: RightSelect = RightSelect(0)
+    memory_mode: MemoryMode = MemoryMode(0)
+    writeback_mode: WritebackMode = WritebackMode(0)
+    alu_function: AluFunction = AluFunction(0)
+    immediate_format: ImmediateFormat = ImmediateFormat(0)
+    jump_condition: JumpCondition = JumpCondition(0)
+    source_register: Register = Register(0)
+    destination_register: Register = Register(0)
 
 
-def decode_lui(inst: str) -> ControlWord:
-    inst[8] @ inst[14:9] @ inst[7:4] @ Bit(0) * 4
+class BitString:
+    def __init__(self, value: int, width: int = 16):
+        self.width = width
+        self.value = value
 
+    def __str__(self) -> str:
+        return bin(self.value)
+
+    def is_clear(self, index: int) -> bool:
+        assert index < self.width
+        masked = self.value & (1 << index)
+        return masked == 0
+
+    def is_set(self, index: int) -> bool:
+        return not self.is_clear(index)
+
+    def field(self, *, width: int, offset: int) -> "BitString":
+        return BitString(
+            (self.value & ((2**width - 1) << offset)) >> offset, width=width
+        )
+
+    def into[T](self, transform: type[T]) -> T:
+        return transform(self.value)
+
+
+NOP = ControlWord(
+    left_select=LeftSelect.SourceRegister,
+    right_select=RightSelect.DestinationRegister,
+    memory_mode=MemoryMode.Disabled,
+    writeback_mode=WritebackMode.Disabled,
+    alu_function=AluFunction.Nop,
+    immediate_format=ImmediateFormat.Alu,
+    jump_condition=JumpCondition.Never,
+    destination_register=Register.R0,
+    source_register=Register.R0,
+)
+
+INSTRUCTION_WIDTH = 16
+
+
+def source_register(inst: BitString) -> Register:
+    return inst.field(width=4, offset=4).into(Register)
+
+
+def destination_register(inst: BitString) -> Register:
+    return inst.field(width=4, offset=0).into(Register)
+
+
+def lui(inst: BitString) -> ...:
     return ControlWord(
-        MemoryReadEnable=False,
-        MemoryWriteEnable=False,
-        WritebackEnable=WritebackMode.AluResult,
-        AluOperation=AluOperation.Nop,  # TODO
-        ImmediateMode=ImmediateMode.Lui,
-        JumpCondition=JumpCondition.Never,
+        immediate_format=ImmediateFormat.Lui,
+        destination_register=inst.field(width=4, offset=0).into(Register),
     )
 
 
-def decode_mem8(inst: str) -> ControlWord:
-    ...
+def mem(inst: BitString, format: MemoryOperationFormat) -> ControlWord:
+    if format is MemoryOperationFormat.Word:
+        read = MemoryMode.ReadWord
+        write = MemoryMode.WriteWord
+    else:
+        read = MemoryMode.ReadByte
+        write = MemoryMode.WriteByte
+
+    is_load = inst.is_clear(13)
+    dest = destination_register(inst)
+    source = source_register(inst)
+
+    return ControlWord(
+        immediate_format=ImmediateFormat.Mem,
+        left_select=LeftSelect.SourceRegister,
+        right_select=RightSelect.DestinationRegister,
+        alu_function=AluFunction.Add,
+        destination_register=dest,
+        source_register=source,
+        memory_mode=read if is_load else write,
+        writeback_mode=(
+            WritebackMode.MemoryResult if is_load else WritebackMode.Disabled
+        ),
+    )
 
 
-def decode_mem16(inst: str) -> ControlWord:
-    ...
+def alu2(inst: BitString) -> ControlWord:
+    dest = destination_register(inst)
+    source = source_register(inst)
+
+    left_select = LeftSelect.SourceRegister
+    right_select = RightSelect.DestinationRegister
+
+    match inst.field(width=4, offset=8).into(int):
+        case 0b0000:
+            func = AluFunction.Lhs
+        case 0b0001:
+            func = AluFunction.Add
+        case 0b0010:
+            func = AluFunction.Addc
+        case 0b0011:
+            func = AluFunction.Sub
+        case 0b0100:
+            func = AluFunction.Subb
+        case 0b0101:
+            func = AluFunction.And
+        case 0b0110:
+            func = AluFunction.Or
+        case 0b0111:
+            func = AluFunction.Xor
+        case 0b1000:
+            func = AluFunction.Shl
+        case 0b1001:
+            func = AluFunction.Lsr
+        case 0b1010:
+            func = AluFunction.Asr
+        case 0b1011:
+            func = AluFunction.Sub
+        case 0b1100:
+            func = (
+                AluFunction.Lhs
+            )  # TODO: 0b1100 represents mv vec, gpr and mv gpr, vec. For now, this is just a normal mv gpr
+        case 0b1101:
+            # TODO: Handle immediate mode functions
+            func = AluFunction.Shl
+        case 0b1110:
+            # TODO: Handle immediate mode functions
+            func = AluFunction.Lsr
+        case 0b1111:
+            # TODO: Handle immediate mode functions
+            func = AluFunction.Asr
+        case _:
+            assert False
+
+    return ControlWord(
+        immediate_format=ImmediateFormat.Alu,
+        alu_function=func,
+        left_select=left_select,
+        right_select=right_select,
+        destination_register=dest,
+        source_register=source,
+        memory_mode=MemoryMode.Disabled,
+        writeback_mode=WritebackMode.AluResult,
+    )
 
 
-def decode_alu2(inst: str) -> ControlWord:
-    ...
+def op11(inst: BitString) -> ControlWord:
+    dest = destination_register(inst)
+    source = source_register(inst)
+
+    match inst.field(width=2, offset=9).into(int):
+        case 0b00:
+            # This is lli
+            return ControlWord(
+                left_select=LeftSelect.SourceRegister,
+                right_select=RightSelect.Immediate,
+                immediate_format=ImmediateFormat.Alu,
+                memory_mode=MemoryMode.Disabled,
+                writeback_mode=WritebackMode.AluResult,
+                jump_condition=JumpCondition.Never,
+                destination_register=dest,
+                source_register=source,
+                alu_function=AluFunction.Rhs,
+            )
+        case 0b01:  # addi
+            return ControlWord(
+                left_select=LeftSelect.SourceRegister,
+                right_select=RightSelect.Immediate,
+                immediate_format=ImmediateFormat.Alu,
+                memory_mode=MemoryMode.Disabled,
+                writeback_mode=WritebackMode.AluResult,
+                jump_condition=JumpCondition.Never,
+                destination_register=dest,
+                source_register=source,
+                alu_function=AluFunction.Add,
+            )
+        case 0b10:  # cmpi
+            return ControlWord(
+                left_select=LeftSelect.SourceRegister,
+                right_select=RightSelect.Immediate,
+                immediate_format=ImmediateFormat.Alu,
+                memory_mode=MemoryMode.Disabled,
+                writeback_mode=WritebackMode.Disabled,
+                jump_condition=JumpCondition.Never,
+                destination_register=dest,
+                source_register=source,
+                alu_function=AluFunction.Sub,
+            )
+        case 0b11:
+            if inst.is_clear(3):  # j
+                return ControlWord(
+                    left_select=LeftSelect.SourceRegister,
+                    right_select=RightSelect.Immediate,
+                    immediate_format=ImmediateFormat.Jmp,
+                    memory_mode=MemoryMode.Disabled,
+                    writeback_mode=WritebackMode.Disabled,
+                    jump_condition=JumpCondition.Always,
+                    destination_register=dest,
+                    source_register=source,
+                    alu_function=AluFunction.Add,
+                )
+            else:  # jal
+                return ControlWord(
+                    left_select=LeftSelect.SourceRegister,
+                    right_select=RightSelect.Immediate,
+                    immediate_format=ImmediateFormat.Jmp,
+                    memory_mode=MemoryMode.Disabled,
+                    writeback_mode=WritebackMode.AluResult,
+                    jump_condition=JumpCondition.Always,
+                    destination_register=Register.LinkRegister,
+                    source_register=source,
+                    alu_function=AluFunction.Add,
+                )
+        case _:
+            assert False
 
 
-def decode_alu1_imm_wide(inst: str) -> ControlWord:
-    ...
+def branch(inst: BitString) -> ControlWord:
+    if inst.is_clear(9):
+        # TODO: This is mv mmu, gpr or mv gpr, mmu
+        return NOP
+
+    match inst.field(width=3, offset=0).into(int):
+        case 0b000:
+            condition = JumpCondition.Always
+        case 0b001:
+            condition = JumpCondition.Always
+        case 0b010:
+            condition = JumpCondition.Equal
+        case 0b011:
+            condition = JumpCondition.NotEqual
+        case 0b100:
+            condition = JumpCondition.LessThan
+        case 0b101:
+            condition = JumpCondition.GreaterEqual
+        case 0b110:
+            condition = JumpCondition.SignedLessThan
+        case 0b111:
+            condition = JumpCondition.SignedGreaterEqual
+        case _:
+            assert False
+
+    return ControlWord(
+        left_select=LeftSelect.ProgramCounter,
+        right_select=RightSelect.Immediate,
+        memory_mode=MemoryMode.Disabled,
+        writeback_mode=WritebackMode.Disabled,  # TODO: enable for JAL / JALR
+        alu_function=AluFunction.Add,
+        immediate_format=ImmediateFormat.JmpR,
+        jump_condition=condition,
+        destination_register=Register.LinkRegister,
+        source_register=Register.R0,
+    )
 
 
-def decode_branch(inst: str) -> ControlWord:
-    ...
+def bitwise_imm(inst: BitString) -> ControlWord:
+    function = AluFunction.And if inst.is_clear(8) else AluFunction.Or
+
+    return ControlWord(
+        alu_function=function,
+    )
 
 
-def decode_alu1_imm(inst: str) -> ControlWord:
-    ...
+def stz(inst: BitString) -> ControlWord:
+    return ControlWord(
+        left_select=LeftSelect.SourceRegister,
+        right_select=RightSelect.DestinationRegister,
+        immediate_format=ImmediateFormat.Stz,
+        memory_mode=MemoryMode.WriteWord,
+        writeback_mode=WritebackMode.Disabled,
+        alu_function=AluFunction.Nop,
+        jump_condition=JumpCondition.Never,
+        destination_register=Register.R0,
+        source_register=source_register(inst),
+    )
 
 
-def decode_stz(inst: str) -> ControlWord:
-    ...
+def alu1(inst: BitString) -> ControlWord:
+    dest = destination_register(inst)
+    source = source_register(inst)
+
+    match inst.field(width=3, offset=4).into(int):
+        case 0b000:  # not
+            function = (
+                AluFunction.Or
+            )  # TODO! Need NOT as a function, or need to be able to produce xor -1
+        case 0b001:  # apc
+            function = AluFunction.Add
+        case 0b010:  # neg
+            function = AluFunction.Sub  # TODO: Probably not right just guessing rn
+        case 0b011:  # negb
+            function = AluFunction.Subb  # TODO: probably not right just guessing rn
+        case 0b100:  # swap
+            function = AluFunction.Swap
+        case 0b101:  # sxt
+            function = AluFunction.Sxt
+        case 0b110:  # unused
+            return NOP
+        case 0b111:  # unused
+            return NOP
+        case _:
+            assert False
+
+    return ControlWord(
+        left_select=LeftSelect.SourceRegister,
+        right_select=RightSelect.DestinationRegister,
+        memory_mode=MemoryMode.Disabled,
+        writeback_mode=WritebackMode.AluResult,
+        alu_function=function,  # TODO
+        immediate_format=ImmediateFormat.Alu,
+        jump_condition=JumpCondition.Never,
+        destination_register=dest,
+        source_register=source,
+    )
 
 
-def decode_alu1(inst: str) -> ControlWord:
-    ...
+def spr(_inst: BitString) -> ControlWord:
+    return NOP
 
 
-def decode_interrupt(inst: str) -> ControlWord:
-    ...
+def op5(_inst: BitString) -> ControlWord:
+    return NOP
 
 
-def decode_zero_op(inst: str) -> ControlWord:
-    ...
+def env(_inst: BitString) -> ControlWord:
+    return NOP
 
 
-def instructions():
-    for val in range(2**16):
-        split = list(bin(val)[2:])
-        while len(split) != 16:
-            split.insert(0, "0")
-        yield "".join(split)
+def interrupt(_inst: BitString) -> ControlWord:
+    return NOP
+
+
+def zero(_inst: BitString) -> ControlWord:
+    return NOP
+
+
+def decode_instruction(instruction: int) -> ControlWord:
+    bits = BitString(instruction)
+    if bits.is_clear(15):
+        return lui(bits)
+    if bits.is_clear(14):
+        return mem(bits, MemoryOperationFormat.Byte)
+    if bits.is_clear(13):
+        return mem(bits, MemoryOperationFormat.Word)
+    if bits.is_clear(12):
+        return alu2(bits)
+    if bits.is_clear(11):
+        return op11(bits)
+    if bits.is_clear(10):
+        return branch(bits)
+    if bits.is_clear(9):
+        return bitwise_imm(bits)
+    if bits.is_clear(8):
+        return stz(bits)
+    if bits.is_clear(7):
+        return alu1(bits)
+    if bits.is_clear(6):
+        return spr(bits)
+    if bits.is_clear(5):
+        return op5(bits)
+    if bits.is_clear(4):
+        return env(bits)
+    if bits.is_clear(3):
+        return interrupt(bits)
+    else:
+        return zero(bits)
 
 
 control_words = list[ControlWord]()
+for instruction in range(2**INSTRUCTION_WIDTH):
+    word = decode_instruction(instruction)
+    control_words.append(word)
 
-for inst in instructions():
-    if inst.startswith("0"):
-        control_words.append(decode_lui(inst))
-    if inst.startswith("10"):
-        control_words.append(decode_mem8(inst))
-    if inst.startswith("110"):
-        control_words.append(decode_mem16(inst))
-    if inst.startswith("1110"):
-        control_words.append(decode_alu2(inst))
-    if inst.startswith("11110"):
-        control_words.append(decode_alu1_imm_wide(inst))
-    if inst.startswith("111110"):
-        control_words.append(decode_branch(inst))
-    if inst.startswith("1111110"):
-        control_words.append(decode_alu1_imm(inst))
-    if inst.startswith("11111110"):
-        control_words.append(decode_stz(inst))
-    if inst.startswith("111111110"):
-        control_words.append(decode_alu1(inst))
-    if inst.startswith("1111111110"):
-        control_words.append(decode_interrupt(inst))
-    if inst.startswith("1111111111"):
-        control_words.append(decode_zero_op(inst))
 
-print(len(control_words) / 1024)
+with open("decoder.bin", "wb") as f:
+    for word in control_words:
+        binary = (
+            word.left_select
+            | (word.right_select << 1)
+            | (word.memory_mode << 2)
+            | (word.writeback_mode << 5)
+            | (word.alu_function << 7)
+            | (word.immediate_format << 11)
+            | (word.jump_condition << 14)
+            | (word.source_register << 17)
+            | (word.destination_register << 21)
+        )
+        f.write(binary.to_bytes(4, byteorder="little"))
